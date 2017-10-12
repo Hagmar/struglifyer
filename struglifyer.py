7369567bbfa1a7f0904d6c4c1de5d368df4fd6ce
Magnus Hagmar <mhagmar@palantir.com>
2017-10-12 09:47:30 -0400

    Initial version. Supports basic uglifyers

---
+++ struglifyer.py
@@ -0,0 +1,73 @@
+#!/usr/bin/env python3
+
+import argparse
+import logging
+import random
+import re
+
+
+class Struglifyer:
+    def __init__(self, args):
+        config = parse_config(args.config)
+        self.token = config['token']
+        self.uglifyers = config['uglifyers']
+
+
+    def run(self, s, initial=False, iters=1):
+        if initial:
+            s = '"{}"'.format(s)
+
+        for _ in range(iters):
+            uf = random.choice(self.uglifyers)
+            s = re.sub(self.token, s, uf['uglifyer'])
+            print(s)
+
+
+def parse_config(config_file):
+    return {
+        'token': '@x',
+        'uglifyers': [
+            {
+                'uglifyer': '"%s" % @x',
+                'esc': False
+            },
+            {
+                'uglifyer': '"{}".format(@x)',
+                'esc': False
+            },
+            {
+                'uglifyer': '@x[:]',
+                'esc': False
+            },
+            {
+                'uglifyer': '@x[::-1][::-1]',
+                'esc': False
+            },
+            {
+                'uglifyer': '"%%%s"%"s"%@x',
+                'esc': False
+            },
+            {
+                'uglifyer': '"".join(c for c in @x)',
+                'esc': False
+            },
+        ]
+    }
+
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--config', help='Configuration file')
+    parser.add_argument('--iters', type=int, default=1, help='Number of iterations')
+
+    return parser.parse_args()
+
+
+def main():
+    args = parse_args()
+    su = Struglifyer(args)
+    su.run("Hello, World!", initial=True, iters=args.iters)
+
+
+if __name__ == '__main__':
+    main()
