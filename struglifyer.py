3c29c3a0ca86f91d93f5569e3483d5ff858f29d9
Magnus Hagmar <mhagmar@palantir.com>
2017-10-12 21:46:32 -0400

    Removed unnecessary configuration parameters

--- struglifyer.py
+++ struglifyer.py
@@ -1,17 +1,17 @@
 #!/usr/bin/env python3
 
+from collections import defaultdict
 import argparse
 import logging
 import random
 import re
+import string
 
 
 class Struglifyer:
     def __init__(self, args):
         config = parse_config(args.config)
-        self.token = config['token']
         self.uglifyers = config['uglifyers']
-
 
     def run(self, s, initial=False, iters=1):
         if initial:
@@ -19,42 +19,23 @@ class Struglifyer:
 
         for _ in range(iters):
             uf = random.choice(self.uglifyers)
-            if uf['esc']:
+            if uf.get('esc'):
                 s = re.sub(r'(\\|")', r'\\\1', s)
 
-            format_dict = { 's': s }
+            format_dict = {'s': s}
             s = uf['uglifyer'].format(**format_dict)
             print(s)
 
 
 def parse_config(config_file):
     return {
-        'token': '@x',
         'uglifyers': [
-            {
-                'uglifyer': '"%s" % {s}',
-                'esc': False
-            },
-            {
-                'uglifyer': '"{{}}".format({s})',
-                'esc': False
-            },
-            {
-                'uglifyer': '{s}[:]',
-                'esc': False
-            },
-            {
-                'uglifyer': '{s}[::-1][::-1]',
-                'esc': False
-            },
-            {
-                'uglifyer': '"%%%s"%"s"%{s}',
-                'esc': False
-            },
-            {
-                'uglifyer': '"".join(c for c in {s})',
-                'esc': False
-            },
+            {'uglifyer': '"%s" % {s}'},
+            {'uglifyer': '"{{}}".format({s})'},
+            {'uglifyer': '{s}[:]'},
+            {'uglifyer': '{s}[::-1][::-1]'},
+            {'uglifyer': '"%%%s"%"s"%{s}'},
+            {'uglifyer': '"".join(c for c in {s})'},
             {
                 'uglifyer': 'eval("{s}")',
                 'esc': True
@@ -65,9 +46,11 @@ def parse_config(config_file):
 
 def parse_args():
     parser = argparse.ArgumentParser()
-    parser.add_argument('-s', '--string', default='Hello, World!', help='The string')
+    parser.add_argument('-s', '--string', default='Hello, World!',
+                        help='The string')
     parser.add_argument('--config', help='Configuration file')
-    parser.add_argument('--iters', type=int, default=1, help='Number of iterations')
+    parser.add_argument('--iters', type=int, default=1,
+                        help='Number of iterations')
 
     return parser.parse_args()
 
